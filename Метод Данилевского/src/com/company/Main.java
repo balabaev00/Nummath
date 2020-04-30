afbff0c7798f73b5309eaabf1dda25311fa2d876
balabaev00 <balabaev00@yandex.ru>
2020-04-30 19:20:10 +0900

    rewrite method Danilevskogo

--- Метод Данилевского/src/com/company/Main.java
+++ Метод Данилевского/src/com/company/Main.java
@@ -1,63 +1,89 @@
 package com.company;
 
+
 public class Main {
 
-    private static double[][] MINV(double [][]A, int N)
+    static final int N = 4;
+
+    static void getCofactor(double A[][], double temp[][], double p, double q, int n)
     {
-        double temp;
+        int i = 0, j = 0;
 
-        double [][] E = new double [N][N];
-
-
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-            {
-                E[i][j] = 0f;
-
-                if (i == j)
-                    E[i][j] = 1f;
-            }
-
-        for (int k = 0; k < N; k++)
+        for (int row = 0; row < n; row++)
         {
-            temp = A[k][k];
-
-            for (int j = 0; j < N; j++)
+            for (int col = 0; col < n; col++)
             {
-                A[k][j] /= temp;
-                E[k][j] /= temp;
-            }
-
-            for (int i = k + 1; i < N; i++)
-            {
-                temp = A[i][k];
-
-                for (int j = 0; j < N; j++)
+                if (row != p && col != q)
                 {
-                    A[i][j] -= A[k][j] * temp;
-                    E[i][j] -= E[k][j] * temp;
+                    temp[i][j++] = A[row][col];
+
+                    if (j == n - 1)
+                    {
+                        j = 0;
+                        i++;
+                    }
                 }
             }
         }
+    }
 
-        for (int k = N - 1; k > 0; k--)
+    static double determinant(double A[][], int n)
+    {
+        double D = 0;
+        if (n == 1)
+            return A[0][0];
+        double [][]temp = new double[N][N];
+        double sign = 1;
+        for (int f = 0; f < n; f++)
         {
-            for (int i = k - 1; i >= 0; i--)
-            {
-                temp = A[i][k];
+            getCofactor(A, temp, 0, f, n);
+            D += sign * A[0][f] * determinant(temp, n - 1);
 
-                for (int j = 0; j < N; j++)
-                {
-                    A[i][j] -= A[k][j] * temp;
-                    E[i][j] -= E[k][j] * temp;
-                }
+            sign = -sign;
+        }
+
+        return D;
+    }
+
+    static void adjoint(double A[][],double [][]adj)
+    {
+        if (N == 1)
+        {
+            adj[0][0] = 1;
+            return;
+        }
+
+        double sign = 1;
+        double [][]temp = new double[N][N];
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                getCofactor(A, temp, i, j, N);
+                sign = ((i + j) % 2 == 0)? 1: -1;
+                adj[j][i] = (sign)*(determinant(temp, N-1));
             }
         }
+    }
+
+    static boolean MINV(double A[][], double [][]inverse)
+    {
+        double det = determinant(A, N);
+        if (det == 0)
+        {
+            System.out.print("Singular matrix, can't find its inverse");
+            return false;
+        }
+
+        double [][]adj = new double[N][N];
+        adjoint(A, adj);
 
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
-                A[i][j] = E[i][j];
-            return A;
+                inverse[i][j] = adj[i][j]/(double)det;
+
+        return true;
     }
 
     public static double[][] MMUL(double[][] matrixOne, double[][] matrixTwo, int n) {
@@ -85,7 +111,7 @@ public class Main {
     }
 
     public static double[][] methodDanilevskogo(double[][] array, int n) {
-        double[][] e;
+        double[][] e = new double[0][];
         for (int i = 1; i < n; i++) {
             e=generateOneMatrix(n);
             for (int j = 0; j < n; j++) {
@@ -97,7 +123,11 @@ public class Main {
                 }
             }
             printMatrix(e,n);
-            array=MMUL(MMUL(MINV(e,n),array,n),e,n);
+            double[][] inv = new double[n][n];
+            MINV(e,inv);
+            double[][] mul1 = MMUL(inv,array,n);
+            double[][] mul2 = MMUL(mul1,e,n);
+            array=mul2;
         }
         return array;
     }
@@ -114,7 +144,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        double[][] array = {{1,2,3,4},{2,1,2,3},{3,2,1,-2},{-4,3,2,1}};
+        //double[][] array = {{1,2,3,4},{2,1,2,3},{3,2,1,-2},{-4,3,2,1}};
+        double[][] array = {{-1,-5,-3,1,-4},{-2,3,-2,2,4},{1,4,4,-5,-3},{-4,-3,-3,5,-1}, {0,-1,-4,-2,5}};
         int n = 4;
         array = methodDanilevskogo(array,n);
         printMatrix(array,n);
